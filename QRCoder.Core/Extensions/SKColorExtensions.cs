a0956a02e01c273561c26c9b11d78d0366858f01
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2025-07-12 22:35:30 -0300

    SKColorExtensions

---
+++ QRCoder.Core/Extensions/SKColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using SkiaSharp;
+using System.Globalization;
+
+namespace QRCoder.Core.Extensions
+{
+    public static class SKColorExtensions
+    {
+        public static string ToHex(this SKColor color)
+        {
+            return string.Format(CultureInfo.InvariantCulture, "#{0:X2}{1:X2}{2:X2}{3:X2}", color.Alpha, color.Red, color.Green, color.Blue);
+        }
+    }
+}
